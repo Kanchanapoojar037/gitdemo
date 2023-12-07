1e351c363fa9b7107dec95f7c0a670d1cdc3976b
Your Name <you@example.com>
2023-12-08 01:31:43 +0530

    First commit

---
+++ DsAlgo_Cucumber/src/test/java/TestRunner/MyTestRunner.java
@@ -0,0 +1,20 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"src/test/resources/features"},
+		glue = {"Stepdefinitions", "AppHooks"},
+		//tags ="@Smoke",
+		plugin = {"pretty",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",				
+		}		
+				)
+
+public class MyTestRunner {
+
+}
